822e891658e4e55bb8ee30a900590111dbb8cd96
Evgen Kostenko <evgeniy.kostenko@ardas.biz>
2017-02-01 21:53:32 +0200

    refactor form

--- user_form.py
+++ user_form.py
@@ -1,9 +1,9 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required, Length
+from wtforms.validators import DataRequired, Length
 
 class UserForm(Form):
-    name = StringField('What is you name?', validators=[Required(), 
+    name = StringField('What is you name?', validators=[DataRequired(),
                                                         Length(1, 16)])
     submit = SubmitField('Submit')
 
